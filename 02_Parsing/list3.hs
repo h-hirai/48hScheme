6651b49e0d563ff86cd78f9fc66db84ac5df3852
HIRAI, Hajime <hirai.hajime@gmail.com>
2011-07-08 19:01:11 +0900

    make change to show value

--- 02_Parsing/list3.hs
+++ 02_Parsing/list3.hs
@@ -10,6 +10,7 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+               deriving Show
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -43,7 +44,7 @@ parseExpr = parseAtom
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
                    Left err -> "No match: " ++ show err
-                   Right val -> "Found value"
+                   Right val -> "Found value: " ++ show val
 
 main :: IO ()
 main = do args <- getArgs
